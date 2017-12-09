980f98ff4cab999745afb41382edb4ff24095e1c
Miriam Castillo <castillo.m.mc@gmail.com>
2017-12-09 03:20:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const returnLastTwoDrivers = function  (drivers){
   return drivers.slice(2,4);
 };
 
+const selectingDrivers = [
+  function () { const returnFirstTwoDrivers; },
+  function () { const returnLastTwoDrivers; },
+  ];
